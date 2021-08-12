34a18214a6ed22af80a05c5d1877642e2289d96d
DSdatsme <darsuratwala@gmail.com>
2021-08-13 03:13:26 +0530

    feat: add log fatal handling

--- main.go
+++ main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	"log"
 	"net/http"
+
 	"github.com/DSdatsme/golang-api-deployment/basicserver"
 )
 
-
 func main() {
 	http.HandleFunc("/", basicserver.HelloWorld)
-	http.ListenAndServe(":8080", nil)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
